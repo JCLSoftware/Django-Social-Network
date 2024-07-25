008577fe20d5022b3ed344875d8c74a25b2497d6
JCL <gitlab@itinganet.com.br>
2024-07-25 14:23:08 -0300

    .

--- README.md
+++ README.md
@@ -12,3 +12,5 @@ Just a basic social media prototype made with Django.
 https://djangosocial-network.herokuapp.com/ <br/><br/>
 ![django social network demo thumbnail](https://user-images.githubusercontent.com/40894497/186735837-58588ed4-3130-4ce6-9a08-ce62a63c456a.jpg)
 
+
+
---
+++ run.sh
@@ -0,0 +1,9 @@
+#python -m venv venv
+source venv/bin/activate
+#pip install Twisted
+#pip install -r requirements.txt
+#python manage.py makemigrations
+#python manage.py migrate
+#python manage.py createsuperuser
+#python manage.py collectstatic
+python manage.py runserver 0.0.0.0:8000
